171edb2bb1b102da08e6191c49b6a6c459b5d5c6
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-01 10:35:40 -0600

    Add getAllGames function to HomePage.vue and GamesService.js

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,9 +1,30 @@
 <script setup>
+import { gamesService } from '@/services/GamesService.js';
+import Pop from '@/utils/Pop.js';
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+  getAllGames();
+});
+
+async function getAllGames() {
+  try {
+    await gamesService.getAllGames();
+  }
+  catch (error) {
+    Pop.error(error);
+  }
+}
 
 </script>
 
 <template>
+
   <H1>Alphacritic</H1>
+  <p>Alphacritic is a web application that allows users to search for movies and view their ratings from various
+    sources.</p>
+
 </template>
 
 <style scoped lang="scss"></style>
---
+++ client/src/services/GamesService.js
@@ -0,0 +1,16 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+
+class GamesService {
+
+async  getAllGames() {
+    const response = await api.post('/games')
+    logger.log(response.data)
+    
+
+}
+
+}
+
+export const gamesService = new GamesService()
