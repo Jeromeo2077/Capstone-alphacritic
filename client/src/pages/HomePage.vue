57d18d8ea12a205daff0ad31d83a428362124f79
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-01 15:22:18 -0600

    Refactor HomePage.vue to use computed property for games and import AppState.js

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,12 +1,15 @@
 <script setup>
+import { AppState } from '@/AppState.js';
 import { gamesService } from '@/services/GamesService.js';
 import Pop from '@/utils/Pop.js';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 
 
 onMounted(() => {
   getAllGames();
 });
+
+const games = computed(() => AppState.games)
 
 async function getAllGames() {
   try {
@@ -24,6 +27,8 @@ async function getAllGames() {
   <H1>Alphacritic</H1>
   <p>Alphacritic is a web application that allows users to search for movies and view their ratings from various
     sources.</p>
+
+  {{ games }}
 
 </template>
 
