9109cfe03a4623115474b7d99dc230b4d6674375
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-02 16:27:37 -0600

    Refactor HomePage.vue to update game information display and add release date

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -37,11 +37,12 @@ async function getAllGames() {
   <div class="container-fluid">
     <div class="row">
       <h2>Popular Games</h2>
-      <div v-for="game in games" :key="game.id" class="col-6 col-md-3 g-3">
+      <div v-for="game in games" :key="game.id" class="col-6 col-md-3 g-3 d-flex align-items-stretch">
         <div class="card">
           <img :src="game.background_image" class="card-img-top img-fluid" alt="...">
           <div class="card-body">
             <h5 class="card-title caudex-bold">{{ game.name }}</h5>
+            <p class="card-text">Release Date: {{ game.released }}</p>
           </div>
         </div>
       </div>
