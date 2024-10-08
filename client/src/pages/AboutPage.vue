e6e7c3db0c4713be821d3e7807454a5830f95a31
Kevin Koontz <contactkkoontz@gmail.com>
2024-10-08 16:00:13 -0600

    updated aboutPage template

--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -3,11 +3,72 @@
 </script>
 
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="container">
+    <section class="row">
+
+      <div class="col-md-12">
+        <div class="text-center text-shadow text-light fs-1 fw-bold mt-5">THE ALPHA CREATORS</div>
+      </div>
+    </section>
+
+    <section class="row">
+
+      <div class="col-md-12">
+        <div class="d-flex bg-text p-3 mt-3">
+          <img src="https://i.postimg.cc/Dyn42jwt/image-3.jpg" class="profile-img" alt="">
+          <div class="d-flex flex-column">
+            <div class="fs-1 fw-bold ms-3">CREATOR NAME</div>
+            <div class="fs-6 ms-4">BIO: Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sequi assumenda
+              soluta aliquid a deleniti modi placeat reprehenderit corrupti natus. Molestias optio quod dolor incidunt
+              culpa ratione porro ipsa blanditiis?</div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="row">
+
+      <div class="col-md-12">
+        <div class="d-flex bg-text p-3 mt-3">
+          <img src="https://i.postimg.cc/Dyn42jwt/image-3.jpg" class="profile-img" alt="">
+          <div class="d-flex flex-column">
+            <div class="fs-1 fw-bold ms-3">CREATOR NAME</div>
+            <div class="fs-6 ms-4">BIO: Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sequi assumenda
+              soluta aliquid a deleniti modi placeat reprehenderit corrupti natus. Molestias optio quod dolor incidunt
+              culpa ratione porro ipsa blanditiis?</div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="row">
+
+      <div class="col-md-12">
+        <div class="d-flex bg-text p-3 mt-3">
+          <img src="https://i.postimg.cc/Dyn42jwt/image-3.jpg" class="profile-img" alt="">
+          <div class="d-flex flex-column">
+            <div class="fs-1 fw-bold ms-3">CREATOR NAME</div>
+            <div class="fs-6 ms-4">BIO: Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sequi assumenda
+              soluta aliquid a deleniti modi placeat reprehenderit corrupti natus. Molestias optio quod dolor incidunt
+              culpa ratione porro ipsa blanditiis?</div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <style scoped lang="scss">
+.text-shadow {
+  text-shadow: 1px 1px 3px black
+}
 
+.profile-img {
+  height: 10dvh;
+  width: 10dvh;
+  border-radius: 5dvh;
+}
+
+.bg-text {
+  background-color: rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
 </style>
