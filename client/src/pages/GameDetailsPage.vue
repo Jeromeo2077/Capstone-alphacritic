819092858922cc9a9192d672efc5a8e801ab2af3
Kevin Koontz <contactkkoontz@gmail.com>
2024-10-11 14:52:23 -0600

    update UI gameDetailsPage

--- client/src/pages/GameDetailsPage.vue
+++ client/src/pages/GameDetailsPage.vue
@@ -52,27 +52,29 @@ async function getReviewsByGameId() {
 
 
 <template>
-  <div class="container-fluid" v-if="game">
-    <section class="row">
-      <div class="col-md-7 mt-5">
-        <img :src="game.background_image" class="img-fluid cover-img" alt="">
-      </div>
-      <div class="col-md-5 mt-5">
-        <div class="bg-text pb-3 px-3">
-          <div class="d-flex flex-column">
-            <span class="fs-4 fw-bold">{{ game.name }}</span>
-            <span>Released on: {{ game.released }}</span>
-            <p class="text-small mt-2">{{ game.description }}</p>
-          </div>
-          <div>
-            <div class="d-flex justify-content-between">
-              <span class="fs-4 fw-bold">APLHASCORE: </span>
-              <div class="alphaScore-bg text-center">
-                <span class="fs-1 fw-bold">8</span>
-              </div>
+  <div class="container" v-if="game">
+    <section class="row text-light text-shadow">
+      <div class="d-flex justify-content-between">
+        <div class="col-md-7 p-1 mt-5">
+          <img :src="game.background_image" class="img-fluid cover-img" alt="">
+        </div>
+        <div class="col-md-5 p-1 mt-5">
+          <div class="bg-text pb-3 px-3">
+            <div class="d-flex flex-column">
+              <span class="fs-4 fw-bold">{{ game.name }}</span>
+              <span>Released on: {{ game.released }}</span>
+              <p class="text-small mt-2">{{ game.description }}</p>
             </div>
-            <div class="mt-3">
-              <span class="text-small">Based on {{ reviews.length }} user reviews</span>
+            <div>
+              <div class="d-flex justify-content-between">
+                <span class="fs-4 fw-bold">APLHASCORE: </span>
+                <div class="alphaScore-bg text-center">
+                  <span class="fs-1 fw-bold">8</span>
+                </div>
+              </div>
+              <div class="mt-3">
+                <span class="text-small">Based on {{ reviews.length }} user reviews</span>
+              </div>
             </div>
           </div>
         </div>
@@ -90,14 +92,14 @@ async function getReviewsByGameId() {
     </section>
 
     <section class="row">
-      <div class="col-md-12 text-center">
+      <div class="col-md-12 text-center text-light text-shadow">
         <h1 class="fw-bold text-decoration-underline m-3">ALPHA REVIEWS</h1>
         <h3 class="fw-bold">THE FRONT LINES</h3>
       </div>
     </section>
 
-    <section class="row">
-      <div v-for="reviews in reviews" :key="reviews.id" class="col-md-4">
+    <section class="row bg-text p-3 mx-5">
+      <div v-for="reviews in reviews" :key="reviews.id" class="col-md-3 g-3">
         <div class="text-scale-down">
           <ReviewCard :reviews="reviews" />
         </div>
@@ -111,22 +113,25 @@ async function getReviewsByGameId() {
 
 <style lang="scss" scoped>
 .cover-img {
-  height: 50dvh;
+  height: 100%;
   width: 100%;
   object-fit: cover;
   object-position: center;
+  border-radius: .5rem;
 }
 
 .alphaScore-bg {
   background-color: #BB0A0A;
   height: 10dvh;
   width: 10dvh;
+  border-radius: .5rem;
 }
 
 .bg-text {
-  max-height: 50dvh;
-  background-color: rgba(0, 0, 0, 0.25);
+  // max-height: 50dvh;
+  background-color: rgba(0, 0, 0, 0.5);
   overflow: hidden;
+  border-radius: .5rem;
 }
 
 .create-btn {
@@ -136,12 +141,17 @@ async function getReviewsByGameId() {
   color: white;
   text-shadow: 1px 1px 3px black;
   border: none;
+  border-radius: .5rem;
 }
 
 .profile-img {
   height: 10dvh;
   width: 10dvh;
   border-radius: 5dvh;
+}
+
+.text-shadow {
+  text-shadow: 1px 1px 5px black
 }
 
 .text-small {
